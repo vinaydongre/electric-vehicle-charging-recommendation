7a0412af3f02a344e1c09994d22b444342cafbdc
Rubal Singh <rubalsingh@Rubals-MacBook.local>
2022-07-06 18:28:23 +0530

    functionalities added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:evscrs/login_view.dart';
 import 'package:evscrs/register_view.dart';
+import 'package:evscrs/verify_email_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import "package:flutter_map/flutter_map.dart";
+import 'package:latlong2/latlong.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -31,16 +33,76 @@ class HomePage extends StatelessWidget {
         options: DefaultFirebaseOptions.currentPlatform,
       ),
       builder: (context, snapshot) {
-        /* final user = FirebaseAuth.instance.currentUser;
-          if (user?.emailVerified ?? false) {
-            print("Hello user");
-          } else {
-            return const VerifyEmailView();
-            print("You need to verify the email first");
-          }
-          return const Text('done'); */
-        return const LoginView();
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const Evrv();
+              } else {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (context) => const VerifyEmailView()),
+                );
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('done');
+          default:
+            return const Text('Loading..');
+        }
       },
+    );
+  }
+}
+
+enum MenuAction { logout }
+
+class Evrv extends StatefulWidget {
+  const Evrv({Key? key}) : super(key: key);
+
+  @override
+  State<Evrv> createState() => _EvrvState();
+}
+
+class _EvrvState extends State<Evrv> {
+  final String apiKey = "QPJ5jAA5c7FCvLxltXvV5QXKmSdZGuFW";
+  @override
+  Widget build(BuildContext context) {
+    final tomtomHQ = new LatLng(52.376372, 4.908066);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EVRV'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout, child: Text('Logout'))
+              ];
+            },
+          ),
+        ],
+      ),
+      body: Center(
+          child: Stack(
+        children: <Widget>[
+          FlutterMap(
+            options: new MapOptions(center: tomtomHQ, zoom: 13.0),
+            layers: [
+              new TileLayerOptions(
+                urlTemplate: "https://api.tomtom.com/map/1/tile/basic/main/"
+                    "{z}/{x}/{y}.png?key={apiKey}",
+                additionalOptions: {"apiKey": apiKey},
+              )
+            ],
+          )
+        ],
+      )),
     );
   }
 }
